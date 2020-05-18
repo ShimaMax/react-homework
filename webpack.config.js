b69b110f608f9446c1dd5de1092da562084168a6
Maksim_Shymko <maksim_shymko@epam.com>
2020-05-18 17:50:34 +0300

    task 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
+    mode: isDev ? 'development' : 'production',
     entry: "./src/index.js",
     output: {
         path: path.join(__dirname, "/dist"),
         filename: "bundle.js"
     },
+    devtool: isDev ? 'eval-source-map' : 'source-map',
     module: {
         rules: [
             {
@@ -26,5 +30,13 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/index.html"
         })
-    ]
+    ],
+    devServer: {
+        liveReload: false,
+        writeToDisk: false,
+        stats: {
+            modules: false,
+            version: false
+        }
+    }
 };
