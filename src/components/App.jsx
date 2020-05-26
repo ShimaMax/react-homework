9c9ba904a6344b981d636fde665dc9cf0704f85b
Maksim_Shymko <maksim_shymko@epam.com>
2020-05-26 14:17:31 +0300

    add error boundary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import '../styles/App.scss';
 import MainPage from './MainPage/MainPage.jsx';
 import MoviePage from './MoviePage/MoviePage.jsx';
+import ErrorBoundary from "./ErrorBoundary.jsx";
 
 class App extends React.Component {
     render() {
-        // return <MainPage />;
-        return <MoviePage />;
+        return <ErrorBoundary>
+            <MainPage />
+            {/*<MoviePage />*/}
+        </ErrorBoundary>;
     }
 }
 
---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        console.log(error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',
+                backgroundColor: '#424242', color: '#F65261'}}>
+                <h1>Something went wrong.</h1>
+            </div>;
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
